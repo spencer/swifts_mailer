75b3387cd93b3ad7222dad68aa8dd873df356ee9
spencer <hello@spencerturner.co.uk>
2021-05-26 17:38:08 +0100

    Update Mailer Spec

--- spec/mailer_spec.rb
+++ spec/mailer_spec.rb
@@ -5,23 +5,22 @@ describe Mailer do
   let(:config) {'spec/files/config.yml'}
   let(:test_mailer) {Mailer.new(['test@example.com', 'test2@example.com'],config)}
   it "needs a list of subscribers" do
-    test_mailer.subscribers.should_not be nil
+    expect(test_mailer.subscribers).not_to be nil
   end
 
   it "sends an email" do
-    Pony.stub (:deliver)
-    expect(Pony).to receive(:deliver)
+    allow(Pony).to receive(:deliver)
     test_mailer.send_email('email', 'hello spec')
   end
 
   it "sends to each of subscribers" do
-    Pony.stub (:deliver)
-    expect(test_mailer).to receive(:send_email).twice
+    allow(Pony).to receive(:deliver)
+    allow(Pony).to receive(:send_email).twice
     test_mailer.send_to_list
   end
 
   it "prints each email to the terminal as it sends" do
-    Pony.stub (:deliver)
+    allow(Pony).to receive(:deliver)
     @output = StringIO.new
     test_mailer.output = @output
     test_mailer.send_email('email', 'hello spec')
@@ -29,7 +28,7 @@ describe Mailer do
   end
 
   it "Previews the email" do
-    Pony.stub (:deliver)
+    allow(Pony).to receive(:deliver)
     test_mailer.preview
   end
 end
